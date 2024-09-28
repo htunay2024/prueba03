013f2022d49370b6b5d4ffd8df22225ac475b61c
htunay2024 <harvytunay2020@gmail.com>
2024-09-28 17:19:11 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    if (window.location.pathname.endsWith("index.html")) {
+        alert("¡Bienvenido a La Lolita! Explore nuestros servicios y productos.");
+        console.log("Bienvenido a La Lolita");
+    }
+};
